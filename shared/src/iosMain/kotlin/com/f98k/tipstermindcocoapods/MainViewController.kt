097634ac856f05208ba9cfabfbd5bb5bf88e566a
fernandomenezes <fernandoamaral1998@gmail.com>
2025-03-22 18:00:53 -0300

    running on ios but crashes

---
+++ shared/src/iosMain/kotlin/com/f98k/tipstermindcocoapods/MainViewController.kt
@@ -0,0 +1,22 @@
+package com.f98k.tipstermindcocoapods
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.window.ComposeUIViewController
+import com.f98k.tipstermindcocoapods.domain.RemoteConfigServiceProvider
+
+
+fun MainViewController() = ComposeUIViewController(
+    configure = {
+        val remoteConfig = RemoteConfigServiceProvider.remoteConfigService
+        remoteConfig?.getRemoteConfigValue("general_matches_hml") { json ->
+        }
+    }
+) {
+    GreetingView("Hello, iOS!")
+}
+
+@Composable
+fun GreetingView(text: String) {
+    Text(text = text)
+}
