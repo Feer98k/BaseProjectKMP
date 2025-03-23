b0d75859039360732ff4ecf37e5157d2856a6475
fernandomenezes <fernandoamaral1998@gmail.com>
2025-03-23 20:05:31 -0300

    add Home feature components and update dependencies

--- shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/domain/usecase/UserUseCase.kt
+++ shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/domain/usecase/UserUseCase.kt
@@ -1,5 +1,7 @@
 package com.f98k.tipstermindcocoapods.domain.usecase
 
+import com.f98k.tipstermindcocoapods.data.repository.UserRepository
+
 class UserUseCase(private val userRepository: UserRepository) {
 
 }
