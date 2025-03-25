0f00bd96b6ea845b060b88cc6366b0509d81ce13
fernandomenezes <fernandoamaral1998@gmail.com>
2025-03-25 19:57:37 -0300

    refactor HomeScreen and extract HomeScreenContent component for improved code organization

--- shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package com.f98k.tipstermindcocoapods
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
---
+++ shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/HomeScreenContent.kt
@@ -0,0 +1,51 @@
+package com.f98k.tipstermindcocoapods.ui.screen
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.f98k.tipstermindcocoapods.ui.components.TipsterText
+import com.f98k.tipstermindcocoapods.ui.state.HomeUiState
+
+
+@Composable
+fun HomeScreenContent(
+    uiState: HomeUiState
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        LazyColumn {
+            item {
+                Button(
+                    onClick = {
+                        uiState.getRemoteConfigData()
+                    },
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .clickable { }
+                ) {
+                    TipsterText(
+                        text = "Get Data !",
+                        color = Color.White
+                    )
+                }
+                Spacer(modifier = Modifier.padding(16.dp))
+                TipsterText(
+                    text = uiState.jsonData,
+                    color = Color.White
+                )
+            }
+
+        }
+    }
+}
