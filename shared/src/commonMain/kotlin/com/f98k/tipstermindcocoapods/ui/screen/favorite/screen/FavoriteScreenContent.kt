6165dc6173432e6e8aaa197fb8474161baeefa87
fernandomenezes <fernandoamaral1998@gmail.com>
2025-03-26 21:24:12 -0300

    add composable functions for Favorite, IaTips, Search, and Settings screens with UI layout

--- shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/favorite/screen/FavoriteScreenContent.kt
+++ shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/favorite/screen/FavoriteScreenContent.kt
@@ -1,2 +1,43 @@
 package com.f98k.tipstermindcocoapods.ui.screen.favorite.screen
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.f98k.tipstermindcocoapods.ui.components.TipsterText
+import com.f98k.tipstermindcocoapods.ui.screen.favorite.state.FavoriteUiActions
+import com.f98k.tipstermindcocoapods.ui.screen.favorite.state.FavoriteUiState
+import com.f98k.tipstermindcocoapods.ui.theme.TipsterTextTypeEnum
+
+
+@Composable
+fun FavoriteScreenContent(
+    uiState: FavoriteUiState,
+    uiActions: FavoriteUiActions
+
+){
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        LazyColumn {
+            item {
+                Spacer(modifier = Modifier.padding(16.dp))
+                TipsterText(
+                    modifier = Modifier.fillMaxSize(),
+                    text = "Favorite Section",
+                    color = Color.White,
+                    type = TipsterTextTypeEnum.Body,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
--- shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/iatips/screen/IaTipsScreenContent.kt
+++ shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/iatips/screen/IaTipsScreenContent.kt
@@ -1,2 +1,42 @@
 package com.f98k.tipstermindcocoapods.ui.screen.iatips.screen
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Card
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.f98k.tipstermindcocoapods.ui.components.TipsterText
+import com.f98k.tipstermindcocoapods.ui.screen.iatips.state.IaTipsUiActions
+import com.f98k.tipstermindcocoapods.ui.screen.iatips.state.IaTipsUiState
+import com.f98k.tipstermindcocoapods.ui.theme.TipsterTextTypeEnum
+
+
+@Composable
+fun IaTipsScreenContent(
+    uiState: IaTipsUiState,
+    uiActions: IaTipsUiActions
+){
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        LazyColumn {
+            item {
+                Spacer(modifier = Modifier.padding(16.dp))
+                TipsterText(
+                    modifier = Modifier.fillMaxSize(),
+                    text = "Ia Tips Section",
+                    color = Color.White,
+                    type = TipsterTextTypeEnum.Body,
+                    textAlign = TextAlign.Center
+                )
+            }
+        }
+    }
+}
