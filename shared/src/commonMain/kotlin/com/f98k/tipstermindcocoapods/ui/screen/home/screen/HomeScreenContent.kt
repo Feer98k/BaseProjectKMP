689b456fd9e77f395080f3f1d938a3915f299d5f
fernandomenezes <fernandoamaral1998@gmail.com>
2025-03-29 18:25:14 -0300

    enhance home screen match display and add total matches count

--- shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/home/screen/HomeScreenContent.kt
+++ shared/src/commonMain/kotlin/com/f98k/tipstermindcocoapods/ui/screen/home/screen/HomeScreenContent.kt
@@ -1,17 +1,17 @@
 package com.f98k.tipstermindcocoapods.ui.screen.home.screen
 
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.f98k.tipstermindcocoapods.ui.components.TipsterText
 import com.f98k.tipstermindcocoapods.ui.screen.home.state.HomeUiActions
 import com.f98k.tipstermindcocoapods.ui.screen.home.state.HomeUiState
 import com.f98k.tipstermindcocoapods.ui.theme.TipsterTextTypeEnum
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
-import androidx.compose.ui.unit.dp
 
 
 @Composable
@@ -25,14 +25,30 @@ fun HomeScreenContent(
     ) {
         LazyColumn {
             item {
-                uiState.generalMatchList.forEach{ match ->
-                    TipsterText(
-                        text = match.matches[0].homeTeam,
-                        type = TipsterTextTypeEnum.Body
-                    )
+                uiState.generalMatchList.forEach { match ->
+                    match.matches.forEach { actualMatch ->
+                        TipsterText(
+                            text = "${actualMatch.homeTeam} VS ${actualMatch.awayTeam}",
+                            type = TipsterTextTypeEnum.Body
+                        )
+                    }
+
                     Spacer(modifier = Modifier.width(12.dp))
                 }
+
+            kotlin.runCatching {
+                uiState.generalMatchList[0].matches.size
+            }.onSuccess{
+                Spacer(modifier = Modifier.width(14.dp))
+
+                TipsterText(
+                    text = "Total Partidas: ${uiState.generalMatchList[0].matches.size?:0}",
+                    type = TipsterTextTypeEnum.Body
+                )
             }
+
+                }
+
         }
     }
 }
